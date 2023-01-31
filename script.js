25044b55f42e02d229facf7a8f1b55e141456256
Robert Gordon <bobbygrdn777@gmail.com>
2023-01-30 22:52:55 -0800

    changes files for updates to features, ready for deployment

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ const checkboxes = document.querySelectorAll('.list-items input[type="checkbox"]
 const listItems = document.querySelector('.list-items');
 const placeholderItem = document.querySelector('#placeholder')
 const inputText = document.querySelector('#text-input');
+const modifyTextFields = document.querySelectorAll('.change');
 
 // Buttons
 const clearListButton = document.querySelector('#clearList');
 const addButton = document.querySelector('#add');
 const clearFinishedButton = document.querySelector('#clearFinished');
+const modifyButtons = document.querySelectorAll('.modify');
+const deleteButtons = document.querySelectorAll('.delete');
 
 // Helper variables
 let lastChecked;
@@ -65,8 +68,30 @@ const addElement = () => {
     let text = document.createElement('p');
     text.textContent = inputText.value;
 
+    let modifyInput = document.createElement('input');
+    modifyInput.setAttribute('type','text');
+    modifyInput.className = 'change';
+
+    let modifyButton = document.createElement('button');
+    modifyButton.setAttribute('type', 'submit');
+    modifyButton.id = 'modify';
+    modifyButton.className = 'button modify';
+    modifyButton.textContent = 'Modify';
+    modifyButton.addEventListener('click', modifyElement);
+
+    let deleteButton = document.createElement('button');
+    deleteButton.setAttribute('type','submit');
+    deleteButton.id = 'delete';
+    deleteButton.className = 'button delete';
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', deleteElement)
+
+
     parentElement.append(input);
     parentElement.append(text);
+    parentElement.append(modifyInput);
+    parentElement.append(modifyButton);
+    parentElement.append(deleteButton);
 
     listItems.appendChild(parentElement)
     
@@ -74,8 +99,51 @@ const addElement = () => {
     inputText.value = '';
 }
 
+const deleteElement = (e) => {
+    e.target.parentElement.remove();
+}
+
+const modifyElement = (e) => {
+    let oldText = e.target.parentElement.children[1];
+    if(oldText.style.display === 'none') {
+        oldText.style.display = 'flex';
+    } else {
+        oldText.style.display = 'none';
+    }
+
+    let text = e.target.parentElement.children[2];
+    if(text.style.display === 'flex') {
+        text.style.display = 'none';
+    } else {
+        text.style.display = 'flex';
+    }
+
+    if(e.target.style.backgroundColor === 'green') {
+        e.target.style.backgroundColor = 'lightgray'
+    } else {
+        e.target.style.backgroundColor = 'green';
+    }
+
+    text.addEventListener('keydown', submitChange);
+}
+
+const submitChange = (e) => {
+
+    if(e.keyCode == 13) {
+        let oldText = e.target.parentElement.children[1];
+        let text = e.target.parentElement.children[2];
+        let modifyButton = e.target.parentElement.children[3];
+        oldText.textContent = text.value;
+        text.style.display = 'none';
+        oldText.style.display = 'flex';
+        modifyButton.style.backgroundColor = 'lightgray';
+    }
+}
+
 // Event listeners 
 checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck))
 clearListButton.addEventListener('click', clearList);
 clearFinishedButton.addEventListener('click', clearFinished);
-addButton.addEventListener('click', addElement)
+addButton.addEventListener('click', addElement);
+deleteButtons.forEach(deleteButton => deleteButton.addEventListener('click', deleteElement));
+modifyButtons.forEach(modifyButton => modifyButton.addEventListener('click', modifyElement));
